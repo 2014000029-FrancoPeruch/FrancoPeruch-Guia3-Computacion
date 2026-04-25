dad0958d11f8acc5c8f036e5803490e04625b9f2
2014000029-FrancoPeruch <f.peruch.5ddb@gmail.com>
2026-04-24 23:34:17 -0600

    Add files via upload

---
+++ Guia3_Cajas_de_dialogo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia3_cajas_de_dialogo;
+
+/**
+ *
+ * @author frank
+ */
+public class Guia3_Cajas_de_dialogo {
+
+    public static void main(String[] args) {
+        jfrmCaja v1 = new jfrmCaja();
+        v1.setVisible(true);
+    }
+}
